46c2bda91dae6cf87da586f38c7df46e8d8a2131
ZakyHamzah <zakyhamzah125@gmail.com>
2025-02-24 00:27:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ function getFarPosition(containerRect, windowWidth, windowHeight, buttonWidth, b
 }
 
 yesBtn.addEventListener('click', () => {
-    title.innerHTML = "HORE!!, yaudaa mau eskrimm apaaa";
+    title.innerHTML = "HORE!!🥳";
     document.querySelector('img').src = "https://media.tenor.com/gUiu1zyxfzYAAAAi/good-night-kiss-kiss.gif";
     noBtn.style.display = 'none';
     yesBtn.style.display = 'none';
